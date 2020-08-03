634b1ce26f8e8272896036093988e81623412d86
pranav1812 <pranavvohra1812@yahoo.in>
2020-08-03 08:11:50 +0530

    backup1

---
+++ src/firebase.js
@@ -0,0 +1,20 @@
+import * as firebase from 'firebase'
+import 'firebase/auth'
+import 'firebase/firestore'
+import 'firebase/analytics'
+
+var firebaseConfig = {
+    apiKey: "AIzaSyDDpDcCMmJsO1SGfPwaG8jtuucGJXt-ZuA",
+    authDomain: "toaadmin-3e19f.firebaseapp.com",
+    databaseURL: "https://toaadmin-3e19f.firebaseio.com",
+    projectId: "toaadmin-3e19f",
+    storageBucket: "toaadmin-3e19f.appspot.com",
+    messagingSenderId: "966226002863",
+    appId: "1:966226002863:web:e3daa150b282c272bd255a",
+    measurementId: "G-KVY9MQ3HHZ"
+  };
+
+export const fire= firebase.initializeApp(firebaseConfig)
+firebase.analytics()
+export const db= firebase.firestore()
+export const auth= firebase.auth()
